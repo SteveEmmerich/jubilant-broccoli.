bf48f6a9e079893e9ccddd0651b57c28f9c6abc9
Steve Emmerich <sdemmer@Steves-MacBook-Pro.local>
2018-04-17 03:35:41 -0400

    updated settings

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   url: 'http://localhost',
-  port: 3000,
-  originUrl: 'http://localhost:3000',
+  port: process.env.PORT || 3000,
+  originUrl: process.env.NODE_ENV == 'production' ? 'https://sdemmer.herokuapp.com/' : 'http://localhost:3000',
   mongoUrl:'mongodb://example:password@ds241699.mlab.com:41699/mobi',//mongodb:localhost:32768/mobi',
   sharedSecret: 'XXX',
   sessionSecret: 'XXX',
