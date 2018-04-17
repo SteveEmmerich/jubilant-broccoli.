dd422a2b1d1355676d17f3560b05e5b49d1a6a24
Steve Emmerich <sdemmer@Steves-MacBook-Pro.local>
2018-04-17 03:39:09 -0400

    updated to use non-static url

--- hocs/withOothAuth.js
+++ hocs/withOothAuth.js
@@ -1,9 +1,14 @@
-const OothClient = require('ooth-client')
+/*const OothClient = require('ooth-client')
 const withOothNext = require('ooth-client-react-next')
-
 const ooth = new OothClient({
   oothUrl: 'http://localhost:3000/auth',
   standalone: false
+})*/
+import OothClient from 'ooth-client';
+import withOothNext from 'ooth-client-react-next';
+import settings from '../public-settings';
+const ooth = new OothClient({
+  oothUrl: `${settings.url}/auth`
 })
 
 const withOothNextProvider = withOothNext(ooth)
